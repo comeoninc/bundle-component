86e739201ee8ce32c9d25dbdeeca4bc5d596f840
C'mon Inc <43278368+comeoninc@users.noreply.github.com>
2019-05-24 13:16:57 +0700

    update
    add smoothscroll polyfills polymer3

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import smoothscroll from 'smoothscroll-polyfill';
+// kick off the polyfill!
+smoothscroll.polyfill();
+window.__forceSmoothScrollPolyfill__ = true;
 entry: ['./app.scss', './app.js']
 output: {
   filename: 'bundle.js'
