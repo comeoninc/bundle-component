ea6086bd33140129b63805ce5cb1e39f167198e3
C'mon Inc <43278368+comeoninc@users.noreply.github.com>
2019-05-24 14:43:10 +0700

    add polymer serve

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// Use relative paths for peer dependencies
+import 'element';
+
+class OtherElement extends LitElement{
+  render(){
+    return html`
+      <element></element>
+    `;
+  }
+}
+customElements.define('other-element', OtherElement);
+
 import smoothscroll from 'smoothscroll-polyfill';
 // kick off the polyfill!
 smoothscroll.polyfill();
