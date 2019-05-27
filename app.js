fdefa86d5c96279e31cc5185b5237a5607d7cb18
C'mon Inc <43278368+comeoninc@users.noreply.github.com>
2019-05-28 01:57:06 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import smoothscroll from 'smoothscroll-polyfill';
-// kick off the polyfill!
-smoothscroll.polyfill();
-window.__forceSmoothScrollPolyfill__ = true;
-entry: ['./app.scss', './app.js']
-output: {
-  filename: 'bundle.js'
-}
